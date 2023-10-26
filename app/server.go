3f35fc3315207e50b0ccad33112d23d9c5329c8c
vijay <vijaycan@icloud.com>
2023-10-26 11:45:55 -0400

    respond with echo

--- app/server.go
+++ app/server.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 	"strings"
 
 	// Uncomment this block to pass the first stage
@@ -10,46 +9,41 @@ import (
 	"os"
 )
 
-func extractPath(data string) string {
-	parts := strings.Split(data, " ")
-	if len(parts) < 2 {
-		return ""
-	}
-	return parts[1]
-}
-
 func main() {
-	var httpVersion = "HTTP/1.1"
-	var statusAccepted int
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
 	for {
-		con, err := l.Accept()
+		conn, err := l.Accept()
 		if err != nil {
 			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
 		}
 		data := make([]byte, 1024)
-		n, err := con.Read(data)
+		n, err := conn.Read(data)
 		if err != nil {
 			fmt.Println("Error reading data from connection: ", err.Error())
 			os.Exit(1)
 		}
-		fmt.Printf("Read %d bytes", n)
-		path := extractPath(string(data))
-		if path == "/" {
-			statusAccepted = http.StatusOK
+
+		request := strings.Split(string(n), "\r\n")
+		startLine := request[0]
+
+		target := strings.Fields(startLine)[1]
+		if target == "/" {
+			conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+		} else if target[0:6] == "/echo/" {
+			random_str := target[6:]
+			writeBuf := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(random_str), random_str)
+			fmt.Printf("Request: %v\n", request)
+			fmt.Printf("Response: %v\n", writeBuf)
+			conn.Write([]byte(writeBuf))
 		} else {
-			statusAccepted = http.StatusNotFound
+			conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 		}
-		_, err = con.Write([]byte(fmt.Sprintf("%s %d %s\r\n\r\n", httpVersion, statusAccepted, http.StatusText(statusAccepted))))
-		if err != nil {
-			fmt.Println("Error reading data from connection: ", err.Error())
-			os.Exit(1)
-		}
-		con.Close()
+
+		conn.Close()
 	}
 }
