af99556be453a20808eeb68d14c8014dccb60525
vijay <vijaycan@icloud.com>
2023-10-26 10:33:05 -0400

    revert to working one

--- app/server.go
+++ app/server.go
@@ -10,45 +10,28 @@ import (
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-	// Uncomment this block to pass the first stage
-	//
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
-	defer l.Close()
-
-	con, err := l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
-
-	defer con.Close()
-
-	response := "HTTP/1.1 200 OK\r\n\r\n"
-
-	_, err = con.Write([]byte(response))
-
-	if err != nil {
-		fmt.Println("Error sending response:", err)
-	}
-
+	defer conn.Close()
 	buff := make([]byte, 1024)
-	_, err = con.Read(buff)
+	_, err = conn.Read(buff)
 	if err != nil {
 		fmt.Printf("Read error: %s\n", err)
 	}
-
+	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 	request := strings.Split(string(buff), "\r\n")
 	startLine := request[0]
 	if strings.Fields(startLine)[1] == "/" {
-		con.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 	} else {
-		con.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 	}
 }
