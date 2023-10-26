2f418080f0ab6328c48d3249bdb7d2973d78cb83
vijay <vijaycan@icloud.com>
2023-10-26 09:36:49 -0400

    test 200 response

--- app/server.go
+++ app/server.go
@@ -16,12 +16,26 @@ func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
-	 	os.Exit(1)
-	 }
-	
-	 _, err = l.Accept()
-	 if err != nil {
-	 	fmt.Println("Error accepting connection: ", err.Error())
-	 	os.Exit(1)
-	 }
+		os.Exit(1)
+	}
+
+	con, err := l.Accept()
+	if err != nil {
+		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+
+	//      buffer := make([]byte, 1024)
+	//		n, err := con.Read(buffer)
+
+	response := "HTTP/1.1 200 OK\r\n\r\n"
+
+	_, err = con.Write([]byte(response))
+
+	if err != nil {
+		fmt.Println("Error sending response:", err)
+	}
+
+	con.Close()
+
 }
